9b994af4368e4384801dfccf7338091e702c947a
Paul Muriel Biya-Bi <pbi@neb.com>
2020-01-14 16:26:14 -0500

    Get container port from program arguments

--- servlet-container/src/main/java/org/rainbow/catalina/connector/http/HttpConnector.java
+++ servlet-container/src/main/java/org/rainbow/catalina/connector/http/HttpConnector.java
@@ -9,45 +9,49 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class HttpConnector implements Runnable {
-    private static final Logger logger = LoggerFactory.getLogger(HttpConnector.class);
+	private static final Logger logger = LoggerFactory.getLogger(HttpConnector.class);
 
-    private boolean stopped;
-    private final String scheme = "http";
+	private boolean stopped;
+	private final String scheme = "http";
+	private int port;
 
-    @Override
-    public void run() {
-        ServerSocket serverSocket = null;
-        int port = 8080;
+	public HttpConnector(int port) {
+		this.port = port;
+	}
 
-        try {
-            serverSocket = new ServerSocket(port, 1, InetAddress.getByName("127.0.0.1"));
-        } catch (IOException e) {
-            logger.error("An I/O error occurred.", e);
-            System.exit(1);
-        }
+	@Override
+	public void run() {
+		ServerSocket serverSocket = null;
 
-        while (!stopped) {
-            Socket socket = null;
+		try {
+			serverSocket = new ServerSocket(port, 1, InetAddress.getByName("127.0.0.1"));
+		} catch (IOException e) {
+			logger.error("An I/O error occurred.", e);
+			System.exit(1);
+		}
 
-            try {
-                socket = serverSocket.accept();
-            } catch (IOException e) {
-                logger.error("An I/O error occurred.", e);
-                continue;
-            }
+		while (!stopped) {
+			Socket socket = null;
 
-            HttpProcessor processor = new HttpProcessor(this);
-            processor.process(socket);
-        }
-    }
+			try {
+				socket = serverSocket.accept();
+			} catch (IOException e) {
+				logger.error("An I/O error occurred.", e);
+				continue;
+			}
 
-    public void start() {
-        logger.info("Starting the server.");
-        Thread thread = new Thread(this);
-        thread.start();
-    }
+			HttpProcessor processor = new HttpProcessor(this);
+			processor.process(socket);
+		}
+	}
 
-    public String getScheme() {
-        return scheme;
-    }
+	public void start() {
+		logger.info("Starting the server.");
+		Thread thread = new Thread(this);
+		thread.start();
+	}
+
+	public String getScheme() {
+		return scheme;
+	}
 }
--- servlet-container/src/main/java/org/rainbow/catalina/startup/Bootstrap.java
+++ servlet-container/src/main/java/org/rainbow/catalina/startup/Bootstrap.java
@@ -2,19 +2,40 @@ package org.rainbow.catalina.startup;
 
 import org.rainbow.catalina.connector.http.HttpConnector;
 import org.rainbow.catalina.loaders.LibraryLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class Bootstrap {
-    static {
-        try {
-            new LibraryLoader().loadAll();
-        } catch (Throwable e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-    }
+	static {
+		try {
+			new LibraryLoader().loadAll();
+		} catch (Throwable e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
 
-    public static void main(String[] args) {
-        HttpConnector connector = new HttpConnector();
-        connector.start();
-    }
+	private static final Logger logger = LoggerFactory.getLogger(Bootstrap.class);
+
+	public static void main(String[] args) {
+		HttpConnector connector = new HttpConnector(getPort(args));
+		connector.start();
+	}
+
+	private static int getPort(String[] args) {
+		if (args.length >= 2) {
+			for (int i = 0; i < args.length; i++) {
+				if ("--port".equals(args[i])) {
+					String port = args[i + 1];
+					try {
+						return Integer.valueOf(port);
+					} catch (NumberFormatException e) {
+						logger.error(String.format("'%s' is not a invalid port number.", port), e);
+						System.exit(1);
+					}
+				}
+			}
+		}
+		return 8080;
+	}
 }
